575a87078d4cb9282bb8882a7c174aa578777aa2
Jovan Zejnula <jovanzeka97@gmail.com>
2024-02-11 16:30:59 +0100

    Add roles table and role_id field to users table

--- database/migrations/2024_02_13_000000_create_users_table.php
+++ database/migrations/2024_02_13_000000_create_users_table.php
@@ -14,10 +14,15 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('lastname');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->integer('role_id')->unsigned();
+            $table->index('role_id');
+            $table->foreign('role_id')
+                ->references('id')
+                ->on('roles')
+                ->onDelete('cascade');
             $table->timestamps();
         });
     }
